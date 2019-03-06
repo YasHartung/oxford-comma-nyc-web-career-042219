0930406d369f2c151555629f9b46ca3e4d00d99c
Yasmine Hartung <y.hartung@gmail.com>
2019-03-06 09:39:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
   if array.length == 1
     return array.join
-    
+
   elsif array.length == 2
     return array.join(" and ")
 
-  else 
+  else
     last_element = array.pop
     message = array.join(", ")
     message += ", and #{last_element}"
